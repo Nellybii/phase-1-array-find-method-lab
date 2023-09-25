8b34b29c29d35dcbc3f37636395348cb7efb7fff
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-09-25 12:10:12 +0300

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    { year: " 2020", result: "W"},
+    { year: "2021", result: "N/A"},
+    { year: "2022", result: "L"},
+     {year: "2023", result: "S"},
+  ]
+  function superbowlWin(array){
+    for(let item of array){
+        if(item.result === "W"){
+            return item.year;
+        }
+    }
+}
